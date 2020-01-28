d476006c842d226af6c24c76902cd024751ca8b5
EduardoLopes <EduardoLopesDS@gmail.com>
2020-01-28 01:07:33 -0300

    Initial Commit

---
+++ app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+import Home from "./pages/home/Home.js";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default App;
---
+++ app/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./Home.scss";
+
+class Home extends Component {
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
