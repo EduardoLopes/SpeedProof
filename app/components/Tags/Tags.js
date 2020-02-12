ffce108df55742417e230372fa737c7bb480fbcb
EduardoLopes <EduardoLopesDS@gmail.com>
2020-02-12 13:36:02 -0300

    [Tags component] Code refactoring

--- app/components/Tags/Tags.js
+++ app/components/Tags/Tags.js
@@ -11,9 +11,7 @@ export default function Tags(props){
 
   function tagColor(tag){
 
-    let color = "grey";
-
-    color = tagsOnDB.includes(tag) ? 'green' : 'grey';
+    let color = 'grey';
 
     if(tagsOnDB.includes(tag) && tags.includes(tag)){
       color = "green";
@@ -27,54 +25,44 @@ export default function Tags(props){
 
   }
 
-  useEffect(() => {
+  function handleOnSubmit(event){
 
+    setTagsOnDB(tags);
+    setTagsInputValue(tags.toString());
 
-    return () => {
+    electron.ipcRenderer.send('update-tags', {
+      id: props.id,
+      tags: tags.toString()
+    });
 
+  }
 
-    }
+  function handleOnChange(){
 
-  }, []);
+    setTagsInputValue(event.target.value);
+
+    let splitedTags = event.target.value.split(",");
+
+    splitedTags = splitedTags.map((tag) => tag.trim());
+
+    splitedTags = splitedTags.filter((tag) => !(/^\s*$/.test(tag)));
+
+    splitedTags = [...new Set(splitedTags)];
+
+    setTags(splitedTags);
+
+  }
+
+  const allTags = [...new Set([...tags, ...tagsOnDB])];
 
   return (
     <div>
       { props.id && (<Segment>
 
-        <Form onSubmit={(event) => {
-
-          setTagsOnDB(tags);
-          setTagsInputValue(tags.toString());
-
-          electron.ipcRenderer.send('update-tags', {
-            id: props.id,
-            tags: tags.toString()
-          });
-
-        }}>
+        <Form onSubmit={handleOnSubmit}>
             <Grid>
-            <Grid.Column width={12}>
-                <Form.Input
-                  placeholder='Tags'
-                  name='name'
-                  value={tagsInputValue}
-                  onChange={(event) => {
-
-                    setTagsInputValue(event.target.value);
-
-                    let splitedTags = event.target.value.split(",");
-
-                    splitedTags = splitedTags.map((tag) => tag.trim());
-
-                    splitedTags = splitedTags.filter((tag) => !(/^\s*$/.test(tag)));
-
-                    splitedTags = [...new Set(splitedTags)];
-
-                    setTags(splitedTags);
-
-
-                  }}
-                />
+              <Grid.Column width={12}>
+                <Form.Input placeholder='Tags' name='name' value={tagsInputValue} onChange={handleOnChange} />
               </Grid.Column>
               <Grid.Column width={4}>
                 <Form.Button style={{width: '100%'}} color="green" content={tagsOnDB.length === 0 ? 'Add tags' : 'Update tags'} />
@@ -82,10 +70,8 @@ export default function Tags(props){
             </Grid>
         </Form>
         {tags.length !== 0 || tagsOnDB.length !== 0 ? (<Divider/>) : ""}
-        {[...new Set([...tags, ...tagsOnDB])].map((tag, index) => {
-          return (<Label color={tagColor(tag)} className={ styles.tagsLable } key={index}>
-            {tag}
-          </Label>)
+        {allTags.map((tag, index) => {
+          return (<Label color={tagColor(tag)} className={ styles.tagsLable } key={index}> {tag} </Label>)
         })}
 
       </Segment>)}
