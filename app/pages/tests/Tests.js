dbc9880f4e8c025f5deb2fb8bc3a9c3ccd4948e8
EduardoLopes <EduardoLopesDS@gmail.com>
2020-02-12 22:00:42 -0300

    Tests Page] Add Ping chart

--- app/pages/tests/Tests.js
+++ app/pages/tests/Tests.js
@@ -16,6 +16,8 @@ export default function Tests(){
   const [searchByServerName, setSearchByServerName] = useState(true);
   const [chartData, setChartData] = useState([]);
   const [maxValueDownloadUpload, setMaxValueDownloadUpload] = useState(10);
+  const [maxPing, setMaxPing] = useState(10);
+
 
   function receiveData(event, data){
 
@@ -74,23 +76,27 @@ export default function Tests(){
 
     const data = [];
     let maxValue = 10;
+    let maxP = 0;
 
     testsData.forEach((test, index) => {
 
       data.push({
         name: moment(test.timestamp, moment.ISO_8601).format("dddd, MMMM Do YYYY, h:mm:ss a"),
         download: (test.download_bandwidth / 125000).toFixed(2),
-        upload: (test.upload_bandwidth / 125000).toFixed(2)
+        upload: (test.upload_bandwidth / 125000).toFixed(2),
+        ping: test.ping_latency
       });
 
-      maxValue = Math.max(maxValue, (test.download_bandwidth / 125000))
-      maxValue = Math.max(maxValue, (test.upload_bandwidth / 125000))
+      maxValue = Math.max(maxValue, (test.download_bandwidth / 125000));
+      maxValue = Math.max(maxValue, (test.upload_bandwidth / 125000));
+      maxP = Math.max(maxP, test.ping_latency);
 
     });
 
     data.reverse();
 
     setChartData(data);
+    setMaxPing(maxP);
     setMaxValueDownloadUpload(maxValue);
 
   }, [testsData]);
@@ -168,21 +174,37 @@ export default function Tests(){
           <AreaChart data={chartData} >
             <defs>
               <linearGradient id="colorDownload" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#6435c9" stopOpacity={0.8}/>
+                <stop offset="15%" stopColor="#6435c9" stopOpacity={0.8}/>
                 <stop offset="95%" stopColor="#6435c9" stopOpacity={0}/>
               </linearGradient>
               <linearGradient id="colorUpload" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#00b5ad" stopOpacity={0.8}/>
+                <stop offset="15%" stopColor="#00b5ad" stopOpacity={0.8}/>
                 <stop offset="95%" stopColor="#00b5ad" stopOpacity={0}/>
               </linearGradient>
             </defs>
-            <YAxis type='number' width={ Math.floor(maxValueDownloadUpload).toString().length * 12}/>
+            <YAxis stroke="#ccc" type='number' width={ Math.floor(maxValueDownloadUpload).toString().length * 12}/>
             <XAxis dataKey="name" hide={true} />
             <Tooltip />
             {/* <CartesianGrid strokeDasharray="3 3" /> */}
             <Legend />
             <Area type="monotone" dataKey="download" stroke="#6435c9" fillOpacity={1} fill="url(#colorDownload)" />
             <Area type="monotone" dataKey="upload" stroke="#00b5ad" fillOpacity={1} fill="url(#colorUpload)" />
+          </AreaChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer width={"100%"} height={100}>
+          <AreaChart data={chartData} >
+            <defs>
+              <linearGradient id="colorPing" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#2185d0" stopOpacity={0.8}/>
+                <stop offset="95%" stopColor="#2185d0" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+            <YAxis stroke="#ccc" type='number' width={ Math.floor(maxPing).toString().length * 12}/>
+            <XAxis dataKey="name" hide={true} />
+            <Tooltip />
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <Legend />
+            <Area type="monotone" dataKey="ping" stroke="#2185d0" fillOpacity={1} fill="url(#colorPing)" />
           </AreaChart>
         </ResponsiveContainer>
       </Segment>)}
