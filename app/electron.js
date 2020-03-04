c2fb08c754388208ebbc552abe059921310c6196
EduardoLopes <EduardoLopesDS@gmail.com>
2020-03-04 01:18:25 -0300

    Use pathRoot to indicate where to place speedtest download

--- app/electron.js
+++ app/electron.js
@@ -7,6 +7,7 @@ const { spawn } = require('child_process');
 const download = require('download');
 const sha256File = require('sha256-file');
 const fs = require('fs');
+const { rootPath } = require('electron-root-path');
 const DB = require('./DB.js');
 // eslint-disable-next-line no-unused-vars
 const DBConfig = require('./DBConfig.js');
@@ -186,10 +187,10 @@ ipcMain.on('check-speedtest', (event, sppedtestPath) => {
 ipcMain.on('request-speedtest-cli-download', () => {
   download(
     'https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-win64.zip',
-    __dirname,
+    rootPath,
     { extract: true },
   ).then(() => {
-    DBConfig.setSpeedtestPath(path.join(__dirname, '/speedtest.exe'));
+    DBConfig.setSpeedtestPath(path.join(rootPath, '/speedtest.exe'));
     mainWindow.webContents.send('speedtest-downloaded', 'done');
   });
 });
